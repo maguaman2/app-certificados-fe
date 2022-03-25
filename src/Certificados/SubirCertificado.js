d7fea5459693a4d3357ab9c67b4cc555615ed412
IvanRene20 <93146580+IvanRene20@users.noreply.github.com>
2022-03-25 11:07:26 -0500

    cambio de coma por punto y coma

--- src/Certificados/SubirCertificado.js
+++ src/Certificados/SubirCertificado.js
@@ -12,11 +12,11 @@ function SubirCertificado() {
   };
 
   const csvFileToArray = string => {
-    const csvHeader = string.slice(0, string.indexOf("\n")).split(",");
+    const csvHeader = string.slice(0, string.indexOf("\n")).split(";");
     const csvRows = string.slice(string.indexOf("\n") + 1).split("\n");
 
     const array = csvRows.map(i => {
-      const values = i.split(",");
+      const values = i.split(";");
       const obj = csvHeader.reduce((object, header, index) => {
         object[header] = values[index];
         return object;
@@ -25,9 +25,10 @@ function SubirCertificado() {
     });
 
     setArray(array);
-   // console.log(array[0]);
+   
    array.map((item) => (
-   createCertificado(item)
+  createCertificado(item)
+  
     ))
   };
 
